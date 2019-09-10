ff7849d418672ec0792357e6d9679596847a2514
Gert Claassen <gertclaassen@outlook.com>
2019-09-10 14:45:30 +0200

    Create __runScraper.bat

---
+++ __runScraper.bat
@@ -0,0 +1,8 @@
+rem python ./pyPhotographScraper.py "-t" ".jpg" "-s" "c:/" "-d" "f:/scrapedFiles/" "-c" 
+python ./pyscraper.py "-t" ".pdf" "-s" "c:/" "-d" "f:/scrapedFiles/" "-c" 
+
+PAUSE
+
+:DONE
+
+:EOF
